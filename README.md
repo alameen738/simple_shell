12b3e8d3c9d5765d8551cada8eb6a7da38acc467
root <alameeen3748@gmail.com>
2023-11-07 14:45:53 -0800

    README.md

---
+++ README.md
@@ -0,0 +1,5 @@
+A shell is a program that serves as an intermediary between the user and the operating system. It facilitates user interaction with the operating system by receiving and interpreting commands entered by the user, verifying their correctness, and executing them. Shells provide a user interface for utilizing computer resources, including memory. Shells can come in two primary forms: a command-line interface, such as Bash, or a graphical user interface, like Microsoft Windows Office.
+
+Step 1: Grasp the Fundamental Concepts
+
+Before embarking on the development of your own shell, it's essential to grasp the foundational concepts of shell operation. You should be familiar with concepts such as input and output processes, file descriptors, and system calls. At system startup, three fundamental data streams are established: stdin, stdout, and stderr.
--- shell.h
+++ shell.h
@@ -1,6 +1,7 @@
 #ifndef _SHELL_H_
 #define _SHELL_H_
 
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -12,10 +13,12 @@
 #include <fcntl.h>
 #include <errno.h>
 
+
 /* For Readswrite buffers */
 #define READ_BUF_SIZE 1024
 #define WRITE_BUF_SIZE 1024
 #define BUF_FLUSH -1
+
 
 /* For command chain*/
 #define CMD_NORM	0
@@ -23,16 +26,20 @@
 #define CMD_AND		2
 #define CMD_CHAIN	3
 
+
 /* for convert_number() */
 #define CONVERT_LOWERCASE	1
 #define CONVERT_UNSIGNED	2
+
 
 /* 1 if using systems getline() */
 #define USE_GETLINE 0
 #define USE_STRTOK 0
 
+
 #define HIST_FILE	".simple_shell_history"
 #define HIST_MAX	4096
+
 
 extern char **environ;
 
@@ -43,6 +50,7 @@ extern char **environ;
  * @str: A strings.
  * @next: Points to next node.
  */
+
 typedef struct liststr
 {
 	int num;
@@ -117,13 +125,16 @@ int find_builtin(info_t *);
 void find_cmd(info_t *);
 void fork_cmd(info_t *);
 
+
 /* for toem_parser.c */
 int is_cmd(info_t *, char *);
 char *dup_chars(char *, int, int);
 char *find_path(info_t *, char *, char *);
 
+
 /* for loophsh.c */
 int loophsh(char **);
+
 
 /*for  toem_errors.c */
 void _eputs(char *);
@@ -131,11 +142,13 @@ int _eputchar(char);
 int _putfd(char c, int fd);
 int _putsfd(char *str, int fd);
 
+
 /* for toem_string.c */
 int _strlen(char *);
 int _strcmp(char *, char *);
 char *starts_with(const char *, const char *);
 char *_strcat(char *, char *);
+
 
 /* for toem_string1.c */
 char *_strcpy(char *, char *);
@@ -143,28 +156,34 @@ char *_strdup(const char *);
 void _puts(char *);
 int _putchar(char);
 
+
 /* for toem_exits.c */
 char *_strncpy(char *, char *, int);
 char *_strncat(char *, char *, int);
 char *_strchr(char *, char);
 
+
 /* for toem_tokenizer.c */
 char **strtow(char *, char *);
 char **strtow2(char *, char);
+
 
 /* for toem_realloc.c */
 char *_memset(char *, char, unsigned int);
 void ffree(char **);
 void *_realloc(void *, unsigned int, unsigned int);
 
+
 /* for toem_memory.c */
 int bfree(void **);
+
 
 /* for toem_atoi.c */
 int interactive(info_t *);
 int is_delim(char, char *);
 int _isalpha(int);
 int _atoi(char *);
+
 
 /* for toem_errors1.c */
 int _erratoi(char *);
@@ -173,24 +192,29 @@ int print_d(int, int);
 char *convert_number(long int, int, int);
 void remove_comments(char *);
 
+
 /* for toem_builtin.c */
 int _myexit(info_t *);
 int _mycd(info_t *);
 int _myhelp(info_t *);
 
+
 /* for toem_builtin1.c */
 int _myhistory(info_t *);
 int _myalias(info_t *);
+
 
 /* for toem_getline.c */
 ssize_t get_input(info_t *);
 int _getline(info_t *, char **, size_t *);
 void sigintHandler(int);
 
+
 /* for toem_getinfo.c */
 void clear_info(info_t *);
 void set_info(info_t *, char **);
 void free_info(info_t *, int);
+
 
 /* for toem_environ.c */
 char *_getenv(info_t *, const char *);
@@ -199,10 +223,12 @@ int _mysetenv(info_t *);
 int _myunsetenv(info_t *);
 int populate_env_list(info_t *);
 
+
 /* for toem_getenv.c */
 char **get_environ(info_t *);
 int _unsetenv(info_t *, char *);
 int _setenv(info_t *, char *, char *);
+
 
 /* for toem_history.c */
 char *get_history_file(info_t *info);
@@ -211,6 +237,7 @@ int read_history(info_t *info);
 int build_history_list(info_t *info, char *buf, int linecount);
 int renumber_history(info_t *info);
 
+
 /* for toem_lists.c */
 list_t *add_node(list_t **, const char *, int);
 list_t *add_node_end(list_t **, const char *, int);
@@ -218,12 +245,14 @@ size_t print_list_str(const list_t *);
 int delete_node_at_index(list_t **, unsigned int);
 void free_list(list_t **);
 
+
 /* for toem_lists1.c */
 size_t list_len(const list_t *);
 char **list_to_strings(list_t *);
 size_t print_list(const list_t *);
 list_t *node_starts_with(list_t *, char *, char);
 ssize_t get_node_index(list_t *, list_t *);
+
 
 /* for toem_vars.c */
 int is_chain(info_t *, char *, size_t *);
