42551939ce606431715aab47e5ad47a85a9af74b
root <alameeen3748@gmail.com>
2023-11-12 13:28:12 -0800

    Getinfo file

---
+++ Getinfo.c
@@ -0,0 +1,78 @@
+#include "shell.h"
+
+/**
+* clear_info - clears the fields in the info_t struct
+* @info: pointer to the struct to be cleared
+*/
+void clear_info(info_t *info)
+{
+	info->arg = NULL;
+	info->argv = NULL;
+	info->path = NULL;
+	info->argc = 0;
+}
+
+/**
+ * set_info - Initlstruct
+ * @info: Address struct
+ * @av: Armt vectr
+ */
+void set_info(info_t *info, char **av)
+{
+	int i = 0;
+
+	info->fname = av[0];
+	if (info->arg)
+	{
+		info->argv = strtow(info->arg, " \t");
+		if (!info->argv)
+		{
+			info->argv = malloc(sizeof(char *) * 2);
+			if (info->argv)
+			{
+				info->argv[0] = _strdup(info->arg);
+				info->argv[1] = NULL;
+			}
+		}
+		for (i = 0; info->argv && info->argv[i]; i++)
+			;
+		info->argc = i;
+
+		replace_alias(info);
+		replace_vars(info);
+	}
+}
+
+/**
+ * free_info - Frees memory allocated for fields in an info_t struct.
+ * @info: Pointer to the info_t struct.
+ * @all: True if freeing all fields, otherwise only necessary fields are freed.
+ *
+ * This function releases the memory allocated for the fields within the
+ * provided info_t struct. If the 'all' parameter is set to true, it frees
+ * all fields; otherwise, only essential fields are deallocated.
+ */
+
+void free_info(info_t *info, int all)
+{
+	ffree(info->argv);
+	info->argv = NULL;
+	info->path = NULL;
+	if (all)
+	{
+		if (!info->cmd_buf)
+			free(info->arg);
+		if (info->env)
+			free_list(&(info->env));
+		if (info->history)
+			free_list(&(info->history));
+		if (info->alias)
+			free_list(&(info->alias));
+		ffree(info->environ);
+			info->environ = NULL;
+		bfree((void **)info->cmd_buf);
+		if (info->readfd > 2)
+			close(info->readfd);
+		_putchar(BUF_FLUSH);
+	}
+}
